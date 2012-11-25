d2866cd3f290d7bf8de50cc2230e210027e4e342
weiwei71 <weiwei.ding.pku@gmail.com>
2012-11-24 21:20:39 -0600

    wordcount hadoop test

---
+++ wordcount/src/java/org/weiwei/hadoop/wordcount/WordCountMapper.java
@@ -0,0 +1,22 @@
+package org.weiwei.hadoop.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * @author weiwei
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] elems = line.split("\t");
+        if(elems!=null && elems.length==2) {
+            context.write(new Text(elems[0]), new IntWritable(Integer.parseInt(elems[1])));
+        }
+    }
+}
---
+++ wordcount/src/java/org/weiwei/hadoop/wordcount/WordCountRunner.java
@@ -0,0 +1,51 @@
+package org.weiwei.hadoop.wordcount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * @author weiwei
+ */
+public class WordCountRunner {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        if(args.length!=2){
+            printUsage();
+            System.exit(-1);
+        }
+        Configuration conf = new Configuration();
+        conf.setBoolean("mapred.compress.map.output", true);
+        conf.set("mapred.map.output.compression.codec", "org.apache.hadoop.io.compress.SnappyCodec");
+        Job job = new Job(conf, "wordCount");
+
+        job.setJarByClass(WordCountRunner.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+
+    private static void printUsage() {
+        System.out.print("WordCountRunner input output");
+    }
+}
